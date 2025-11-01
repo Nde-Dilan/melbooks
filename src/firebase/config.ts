562ee277e45b5c1b89c1f4bf57c69e66032be8e1
HURICH DILAN NDE <nde.dilan@ictuniversity.edu.cm>
2025-11-01 21:35:43 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-6318077928-43b3a",
+  "appId": "1:1032976057311:web:647fe5492dfcbc7a2b7a6a",
+  "apiKey": "AIzaSyBthTmRNXyVVpJ9Sqi2pi7VIYSkaG0ytNk",
+  "authDomain": "studio-6318077928-43b3a.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1032976057311"
+};
